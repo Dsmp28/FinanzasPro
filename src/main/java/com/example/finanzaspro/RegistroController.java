684eca73c481e32cb8ad130c6e23e398a748ba8e
erickkrv <rivaal04@gmail.com>
2024-05-19 19:53:34 -0600

    Checkbox pago recurrente

--- src/main/java/com/example/finanzaspro/RegistroController.java
+++ src/main/java/com/example/finanzaspro/RegistroController.java
@@ -2,7 +2,10 @@ package com.example.finanzaspro;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,8 +14,19 @@ public class RegistroController implements Initializable {
     @FXML
     private ChoiceBox<String> cbMovimiento;
 
+    @FXML
+    private CheckBox cbRecurrente;
+
+    @FXML
+    private Label labelDias;
+
+    @FXML
+    private TextField txtDias;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        txtDias.visibleProperty().bind(cbRecurrente.selectedProperty());
+        labelDias.visibleProperty().bind(cbRecurrente.selectedProperty());
         cbMovimiento.getItems().add("Ingreso");
         cbMovimiento.getItems().add("Egreso");
         cbMovimiento.setValue("Ingreso");
