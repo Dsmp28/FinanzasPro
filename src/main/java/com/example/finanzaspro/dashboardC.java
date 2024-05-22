26c5ce97199dcb9c0184ea7e00b29863f6a0cbc7
erickkrv <rivaal04@gmail.com>
2024-05-22 16:52:57 -0600

    Interfaz y lógica editar movimiento + Edición menú lateral + Añadí funcionamiento para botón de salir

--- src/main/java/com/example/finanzaspro/dashboardC.java
+++ src/main/java/com/example/finanzaspro/dashboardC.java
@@ -5,11 +5,18 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.binding.StringBinding;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -33,6 +40,8 @@ public class dashboardC implements Initializable {
         CargarIngresado();
         CargarEgresos();
         CargarPresupuesto();
+
+        lvListaMovimientos.setOnMouseClicked(this::listViewDoubleClick);
     }
 
     private void CargarPresupuesto(){
@@ -58,5 +67,31 @@ public class dashboardC implements Initializable {
         Egreso egreso = ManejadorEgresos.getInstancia().getEgreso();
         StringBinding textoEgresado = Bindings.createStringBinding(() -> String.format("Q. %.2f", egreso.montoProperty().get()), egreso.montoProperty());
         txtGasto.textProperty().bind(textoEgresado);
+    }
+
+    private void listViewDoubleClick(MouseEvent event){
+        if(event.getClickCount() == 2){
+            Movimiento movimiento = lvListaMovimientos.getSelectionModel().getSelectedItem();
+            if(movimiento != null){
+                abrirEdicion(movimiento);
+            }
+        }
+    }
+    private void abrirEdicion(Movimiento movimiento){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("editar-view.fxml"));
+            Parent root = loader.load();
+            Stage emergente = new Stage();
+
+            emergente.initModality(Modality.APPLICATION_MODAL);
+            emergente.setTitle("Editar movimiento");
+            emergente.setScene(new Scene(root));
+            editarController controller = loader.getController();
+            controller.setStage(emergente);
+            controller.setMovimiento(movimiento);
+            emergente.showAndWait();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
